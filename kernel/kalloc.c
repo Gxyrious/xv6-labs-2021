9d56156cfede09c4c7badecf72cf7f9a8e7c62ff
Gxyrious <2466445001@qq.com>
2022-08-13 15:08:07 +0800

    Lab: Copy-on-Write Fork for xv6

---
+++ kernel/kalloc.c
@@ -0,0 +1,105 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+struct {
+  struct spinlock lock;
+  uint pagetable[PHYSTOP >> PGSHIFT];
+} kref;
+
+int
+ksbrk(uint64 pa, int n)
+{
+  int key;
+  acquire(&kref.lock);
+  key = (kref.pagetable[pa >> PGSHIFT] += n);
+  release(&kref.lock);
+  return key;
+}
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  initlock(&kref.lock, "kref");
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    kref.pagetable[(uint64)p >> PGSHIFT] = 1;
+    kfree(p);
+  }
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  if(ksbrk((uint64)pa, -1) > 0)
+    return;
+
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+
+  ksbrk((uint64)r, 1);
+  return (void*)r;
+}
